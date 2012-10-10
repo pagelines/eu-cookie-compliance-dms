d27b4daef19eead3073493e52be8ab35b8a7d4bd
Ryan Varley <ryanjvarley@gmail.com>
2012-10-10 12:55:17 +0100

    converted to LESS, many improvments for 2.0

--- eu-cookie-compliance.js
+++ eu-cookie-compliance.js
@@ -21,31 +21,107 @@ for (i=0;i<ARRcookies.length;i++)
     }
   }
 }
-function EuccCheckCookie(requireAccept){
+function euccCheckCookie(requireAccept){
 var cookieconsent=EuccGetCookie("eucookiecompliance");
-if (cookieconsent == 'implied' || cookieconsent == 'accepted'){
-	jQuery("#eu-cookie-compliance").remove();
-  }
+
+//if (typeof EuccDevMode != 'undefined') {
+    if (cookieconsent == 'implied' || cookieconsent == 'accepted'){
+        //jQuery("#eu-cookie-compliance").remove();
+      }
+//}
 else 
   {
-	jQuery("#eu-cookie-compliance").show();
-	
-	if (requireAccept == 'implied'){
+    jQuery("#eu-cookie-compliance").show();
+    
+    if (requireAccept == 'implied'){
     EuccSetCookie("eucookiecompliance",'implied',365);
-	}
-  }
+    }
 }
+
+jQuery("#eu-cookie-compliance").show(); // temp
+
+
 /* code to hide the notice on click */
-jQuery(document).ready(function() {
-	jQuery(".eucc-closeicon").click(function () {
-	jQuery("#eu-cookie-compliance").remove();
-	});
-});
+    jQuery(".eucc-closeicon").click(function () {
+    jQuery("#eu-cookie-compliance").remove();
+    });
+};
 
 /* code to hide the notice and save cookie if acceptance needed */
 jQuery(document).ready(function() {
-	jQuery(".eucc-hidebutton").click(function () {
-	jQuery("#eu-cookie-compliance").remove();
-	EuccSetCookie("eucookiecompliance",'accepted',365);
-	});
+    jQuery(".eucc-closeicon").click(function () {
+    jQuery("#eu-cookie-compliance").remove();
+    EuccSetCookie("eucookiecompliance",'accepted',365);
+    });
 });
+
+// if bar isnt going to show make the following functions not run without js failing
+
+function euccSetWidth(){
+    var containerWidth = jQuery(".eucc-container").width();
+    var closeIconWidth = jQuery(".eucc-closeicon").width();
+    var paraWidth = jQuery(".eucc-p").width();
+    var newParaWidth = containerWidth - closeIconWidth -12; //20 is the padding
+    
+    if (newParaWidth < paraWidth){ jQuery(".eucc-p").width(newParaWidth); }
+    
+    var iconHeight = jQuery(".eucc-closeicon").height()
+    var iconMargin = (jQuery(".eucc-container").height() - iconHeight)/2;
+    
+    jQuery(".eucc-closeicon").css('margin-top',iconMargin + "px");
+    
+    
+    // document.write('<p>cont: ' + containerWidth + '<br>icon: ' + closeIconWidth + '<br>para: ' + paraWidth 
+    // + '<br>NewPara: ' + newParaWidth + '<br>marg: ' + iconMargin + '<br>iconh: ' + iconHeight + '<br>conth: ' + jQuery(".eucc-container").height()
+    // + '</p> ');
+    }
+    
+function euccPosition(euccPos){ // perhaps call in the function that shows the section
+    
+    if(euccPos == "top-float" || euccPos == "bottom-float") {
+        jQuery("#eu-cookie-compliance").addClass("eucc-float eucc-bgcolor"); // eucc-bgcolor is normal theme
+        jQuery(".eucc-container").removeClass("eucc-bgcolor"); // remove the inner class bg
+        
+        if(euccPos == "top-float"){floatPosition = "top";}
+        if(euccPos == "bottom-float"){floatPosition = "bottom";}
+        
+        jQuery("#eu-cookie-compliance").css(floatPosition,0);
+        
+        var sectionHeight = jQuery("#eu-cookie-compliance").height()
+        jQuery("body").css("margin-"+floatPosition,sectionHeight)
+    }
+    
+    
+}
+
+function euccTheme(euccTheme){
+    
+    // a function or global to return weather float or not so the right class can be selected 
+    
+    addClassName = "#eu-cookie-compliance";
+    addClassName = ".eucc-container";
+
+    if(euccTheme != 'std-flat'){
+    jQuery(".eucc-container").removeClass("eucc-bgcolor");
+    }
+
+    if(euccTheme == 'std-trans'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-trans");
+    }
+
+    if(euccTheme == 'navbar-black'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-black");
+    }
+    if(euccTheme == 'navbar-grey'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-grey");
+    }
+    if(euccTheme == 'navbar-orange'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-orange");
+    }
+    if(euccTheme == 'navbar-blue'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-blue");
+    }
+    if(euccTheme == 'navbar-red'){
+        jQuery(addClassName).addClass("eucc-navbar eucc-red");
+    }
+}
--- section.php
+++ section.php
@@ -1,83 +1,96 @@
 <?php
 /*
-	Section: EU Cookie Compliance
-	Author: Ryan Varley
-	Author URI: http://ryanvarley.co.uk
-	Description: Displays a banner to new visitors about your sites use of cookies.
-	Class Name: EUCookieCompliance
-	Workswith: templates, main, header, morefoot, sidebar1, sidebar2, sidebar_wrap
-	Compatibility: 2.1+
-	Version: 1.0.1
-	External: http://ryanvarley.co.uk/projects/pagelines/eu-cookie-compliance/
-	Demo: http://demo.ryanvarley.co.uk/eu-cookie-compliance/
-	Cloning: false
+    Section: EU Cookie Compliance
+    Author: Ryan Varley
+    Author URI: http://ryanvarley.co.uk
+    Description: Displays a banner to new visitors about your sites use of cookies.
+    Class Name: EUCookieCompliance
+    Workswith: templates, main, header, morefoot, sidebar1, sidebar2, sidebar_wrap
+    Compatibility: 2.2.5+
+    Version: 2.0
+    External: http://ryanvarley.co.uk/projects/pagelines/eu-cookie-compliance/
+    Demo: http://demo.ryanvarley.co.uk/eu-cookie-compliance/
+    Cloning: false
 */
 
 class EUCookieCompliance extends PageLinesSection {    
 
     function section_head(){ # Load the javascript and any associated variables.
-    $euccDevMode = ( ploption('eucc_DevMode', $this->oset) ) ? ploption('eucc_DevMode', $this->oset) : false;
-            if(!$euccDevMode){ ?> <script type='text/javascript' src='<?php echo $this->base_url;?>/eu-cookie-compliance.js?ver=1.0.1'></script><?php }
-        }
+    
+        $devMode = ( ploption('eucc_DevMode', $this->oset) ) ? ploption('eucc_DevMode', $this->oset) : false;
+        
+        $customJs = ''; // initialise
+        
+        // perhaps include in validate variables function which alerts the user to problems?
+        if($devMode != 'True' || $devMode != 'False'){$devMode == 'False'; }
+        $customJs .= sprintf("var EuccDevMode = '%s';",$devMode);
+        
+        // Output user set JS variables and the header script
+        printf("<script type='text/javascript'>
+        %s
+        </script>
+        <script type='text/javascript' src='%s/eu-cookie-compliance.js?ver=2.0'></script>
+        ",$customJs,$this->base_url);
+    }
 
     function section_template( $clone_id = null ) {
-	
-		//set variables for ease
-        $euccBoxText = ( ploption('eucc_BoxText', $this->oset) ) ? ploption('eucc_BoxText', $this->oset) : false;
-        $euccPrivacyPolicyLink = ( ploption('eucc_PrivacyPolicyLink', $this->oset) ) ? ploption('eucc_PrivacyPolicyLink', $this->oset) : false;
-        $euccCloseButtonImage = ( ploption('eucc_CloseButtonImage', $this->oset) ) ? ploption('eucc_CloseButtonImage', $this->oset) : false;
-        $euccAcceptMode = ( ploption('eucc_RequireAccept', $this->oset) ) ? ploption('eucc_RequireAccept', $this->oset) : 'implied';
-        $euccDevMode = ( ploption('eucc_DevMode', $this->oset) ) ? ploption('eucc_DevMode', $this->oset) : false;
-		$euccAcceptButtonText = ( ploption('eucc_AcceptButtonText', $this->oset) ) ? ploption('eucc_AcceptButtonText', $this->oset) : "I Accept";
-		$euccButtonPosition = ( ploption('eucc_ButtonPosition', $this->oset) ) ? ploption('eucc_ButtonPosition', $this->oset) : "top-right";
-		
-		// If needed values arent set then notify user
-        if(!$euccPrivacyPolicyLink && !$euccBoxText){ echo setup_section_notify( $this, 'If your using the default text you must set a link to your cookie information page' ); return;}
-		
-		// Start section
-		
-		// define the close button (or trigger generation later)
-        $euccCustomCloseButton = true;
-  
-        if(!$euccCloseButtonImage){
-        $euccCloseButtonImage = $this->base_url.'/close.png';
-        $euccCustomCloseButton = false;
-        }
+    
+        // Some Defaults + initials
+        $privacyPolicyLink = ( ploption('eucc_PrivacyPolicyLink', $this->oset) ) ? ploption('eucc_PrivacyPolicyLink', $this->oset) : false;
         
-		// a switch for later
-        if($euccAcceptMode == 'acceptance'){ $euccRequireAccept = true; } else{ $euccRequireAccept = false;}
-		
-		// add a class for the bottom-center layout mode
-		if ($euccButtonPosition == 'bottom-center'){$euccExtraButtonClass ='eucc-ownline';}
-		else {$euccExtraButtonClass ='';}
-      
-	  
-		// set the close button variable depending on options
-		
-		//
-		//if ($euccButtonPosition = 'center-right') {$euccCloseButton = '<span class="eucc-hidebutton eucc-center-right" style="background-image:url(\''.$euccCloseButtonImage.'\');"></span>';}
-        if ($euccRequireAccept && !$euccCustomCloseButton) {$euccCloseButton = '<span class="eucc-accept-cookies eucc-hidebutton '.$euccExtraButtonClass.'">'.$euccAcceptButtonText.'</span>';}
-        else{ $euccCloseButton = '<span class="eucc-hidebutton '.$euccExtraButtonClass.'"><img class="eucc-closeicon" src="'.$euccCloseButtonImage.'"/></span>'; } // if implied or custom button output image
+        $defaultText = "This site uses cookies. By continuing to browse the site you are agreeing to our use of cookies. <a href=".$privacyPolicyLink.">Find out more here.</a>";
+    
+        // set variables for ease
+        $boxText = ( ploption('eucc_BoxText', $this->oset) ) ? ploption('eucc_BoxText', $this->oset) : $defaultText;
+        $closeButtonImage = ( ploption('eucc_CloseButtonImage', $this->oset) ) ? ploption('eucc_CloseButtonImage', $this->oset) : false;
+        $acceptMode = ( ploption('eucc_RequireAccept', $this->oset) ) ? ploption('eucc_RequireAccept', $this->oset) : 'implied';
+        $devMode = ( ploption('eucc_DevMode', $this->oset) ) ? ploption('eucc_DevMode', $this->oset) : false;
+        $acceptButtonText = ( ploption('eucc_AcceptButtonText', $this->oset) ) ? ploption('eucc_AcceptButtonText', $this->oset) : "I Accept";
+
+        $bannerPosition = ( ploption('eucc_BannerPosition', $this->oset) ) ? ploption('eucc_BannerPosition', $this->oset) : "normal";
+        $customJs = '';
+        $bannerTheme = ( ploption('eucc_BannerTheme', $this->oset) ) ? ploption('eucc_BannerTheme', $this->oset) : "navbar-black";
+        $customJs = '';
         
-        // start output
-        ?><p><?php
-		
-		if($euccButtonPosition == 'top-right' || $euccButtonPosition == 'center-right') {echo $euccCloseButton;}
-		
-        if ($euccBoxText){
-            printf($euccBoxText);
-        }
-        else{
-            ?>
-            This site uses cookies. By continuing to browse the site you are agreeing to our use of cookies. <a href="<?php echo $euccPrivacyPolicyLink; ?>">Find out more here.</a><?php
-        }
-		if($euccButtonPosition == 'bottom-right' || $euccButtonPosition == 'bottom-center') {echo $euccCloseButton;}
-         ?></p>
+        // If needed values arent set then notify user
+        if(!$privacyPolicyLink && !$boxText){ echo setup_section_notify( $this, 'If your using the default text you must set a link to your cookie information page' ); return;}
         
-        <?php if(!$euccDevMode){ ?><script type='text/javascript'>EuccCheckCookie('<?php echo $euccAcceptMode; ?>');</script><?php } //output script variable
+        // some validation of button positoning
+        
+    // Start section code
+        
+        // Generate Close Button
+        if ($acceptMode == 'acceptance'){ $closeIcon = sprintf('<a class="btn btn-success btn-mini">%s</a>',$acceptButtonText); }
+        else { $closeIcon = '<i class="icon-remove"></i>'; } // another option if pagelines < 2.3 - perhaps a filter for legacy?
+        
+        // final output
+        printf('
+        <div class="eucc-container eucc-bgcolor">
+            <p class="eucc-p">%s</p>  <span class="eucc-closeicon">  %s  </span>
+        </div>
+        ',$boxText, $closeIcon);
+        
+        // after section js
+        
+        if($bannerPosition == 'top-float' || $bannerPosition == 'bottom-float') {
+            $customJs .= sprintf("euccPosition('%s');",$bannerPosition);
+            }
+        
+        if($bannerTheme) {
+            $customJs .= sprintf("euccTheme('%s');",$bannerTheme);
+            }    
+        
+        
+        // Output user set JS variables
+        printf("<script type='text/javascript'>
+        euccCheckCookie('%s');
+        %s
+        euccSetWidth();
+        </script>
+        ",$acceptMode,$customJs);
     }
     
-	
+    
     function section_optionator( $settings ){
             
             $settings = wp_parse_args($settings, $this->optionator_default);
@@ -112,36 +125,50 @@ class EUCookieCompliance extends PageLinesSection {
                                         ), 
                     'shortexp'        => 'You can either show the banner once and if the user takes no action, assume implied consent or you can require the user actually consent to remove the banner.',
                 ),
-				'eucc_ButtonPosition' => array(
-					'default' 		=> 'top-right',
-					'type' 			=> 'radio',
-					'selectvalues' => array(
-						'top-right' 		=> array('name' => 'Top Right (default)'),
-						//'center-right' 		=> array('name' => 'Center Right'), # future
-						'bottom-right' 		=> array('name' => 'Bottom Right'),
-						'bottom-center' 		=> array('name' => 'Bottom Center'),						
-					),
-					'inputlabel' 	=> 'Position',
-					'title' 		=> 'Button Position',
-					'shortexp' 		=> 'Choose where you want the button to display on your banner (this mainly effects multi-line banners)',
-				),
-				'eucc_AcceptButtonText'     => array(
+                'eucc_BannerPosition' => array(
+                    'default'         => 'normal',
+                    'type'             => 'radio',
+                    'selectvalues' => array(
+                        'normal'         => array('name' => 'As a Section (normal)'),
+                        'top-float'         => array('name' => 'Top Float'),
+                        'bottom-float'         => array('name' => 'Bottom Float'),                        
+                    ),
+                    'inputlabel'     => 'Position',
+                    'title'         => 'Banner Position',
+                    'shortexp'         => 'Either contain the banner to its section or allow it to float on the top or bottom of the page.',
+                ),
+                'eucc_AcceptButtonText'     => array(
                     'type'             => 'text',
                     'inputlabel'    => 'Accept button text',
                     'title'         => 'Accept button text (optional)',
                     'shortexp'        => 'The text that will display on the accept button when the \'acceptance\' mode has been selected',
                 ),
+                'eucc_BannerTheme' => array(
+                    'default' => 'std-trans',
+                    'title' => 'Banner Theme',
+                    'shortexp' => 'standard themes match your theme colour - the others are Styled like the NavBar Section', 'eucc_section',
+                    'type' => 'select',
+                    'selectvalues' => array(
+                        'std-flat'             => array('name' => __( 'Standard Flat (default)', 'eucc_section' ) ),
+                        'std-trans'         => array('name' => __( 'Standard gradient', 'eucc_section' ) ),                       
+                        'navbar-black'         => array('name' => __( 'NavBar Black Trans', 'eucc_section' ) ),
+                        'navbar-grey'             => array('name' => __( 'NavBar Light Grey', 'eucc_section' ) ),
+                        'navbar-orange'         => array('name' => __( 'NavBar Orange', 'eucc_section' ) ),
+                        'navbar-blue'         => array('name' => __( 'NavBar Blue', 'eucc_section' ) ), 
+                        'navbar-red'         => array('name' => __( 'NavBar Red', 'eucc_section' ) ),                         
+                    ),
+                ),
                 'eucc_DevMode'     => array(
                     'type'             => 'check',
                     'inputlabel'    => 'Enabled',
                     'title'         => 'Dev Mode',
-                    'shortexp'        => 'This will stop the banner from being permanently hidden for testing purposes. This affects ALL users and you should turn it off before deployment',
+                    'shortexp'        => 'This will stop the banner from being permanently hidden for testing purposes. This affects ALL users and you should TURN IT OFF before deployment or they will see the banner on every page!.',
                 )
             );
 
             $settings = array(
                 'id'         => $this->id.'_meta',
-                'name'         => $this->name,
+                'name'         => 'EU Cookie',
                 'icon'         => $this->icon, 
                 'clone_id'    => $settings['clone_id'], 
                 'active'    => $settings['active']
