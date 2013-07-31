f395d327492b5f41e28bfa70915a385fd377091b
Ryan Varley <ryanjvarley@gmail.com>
2013-07-31 20:28:00 +0100

    restructuring for DMS

---
+++ eu-cookie-compliance.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    Plugin Name: EU Cookie Compliance
+	Author: Ryan Varley
+	Author URI: http://ryanvarley.co.uk
+	Description: Displays a banner to new visitors about your sites use of cookies.
+	Class Name: EUCookieCompliance
+	Workswith: templates, main, header, morefoot, sidebar1, sidebar2, sidebar_wrap
+	Compatibility: 2.3+
+	Version: 2.0
+	External: http://ryanvarley.co.uk/projects/pagelines/eu-cookie-compliance/
+	Demo: http://demo.ryanvarley.co.uk/eu-cookie-compliance/
+	Cloning: false
+    V3: true
+*/
