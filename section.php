cc431d40917cc6e983d8d8fcf637184e6820f400
Ryan Varley <ryanjvarley@gmail.com>
2013-07-31 20:16:10 +0100

    updated versioning and info

--- section.php
+++ section.php
@@ -6,8 +6,8 @@
 	Description: Displays a banner to new visitors about your sites use of cookies.
 	Class Name: EUCookieCompliance
 	Workswith: templates, main, header, morefoot, sidebar1, sidebar2, sidebar_wrap
-	Compatibility: 2.1+
-	Version: 1.0
+	Compatibility: 2.3+
+	Version: 2.0
 	External: http://ryanvarley.co.uk/projects/pagelines/eu-cookie-compliance/
 	Demo: http://demo.ryanvarley.co.uk/eu-cookie-compliance/
 	Cloning: false
